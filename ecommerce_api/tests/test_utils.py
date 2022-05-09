fb8171c0f3f8454f53bf36b45bba1bba647c2223
Tony De La Nuez <tony.delanuez@gmail.com>
2022-05-09 09:43:48 -0500

    Remove user fixture from utils

--- ecommerce_api/tests/test_utils.py
+++ ecommerce_api/tests/test_utils.py
@@ -1,19 +1,8 @@
-import pytest
-from ecommerce.models import User
-from ecommerce.utils import get_cart, user_cart_key
-
-
-@pytest.fixture
-def user():
-    return User(email="test@test.com", password="test")
-
-@pytest.fixture
-def admin_user():
-    return User(email="admin@test.com", password="test", is_admin=True)
+from ecommerce.utils import get_cart
 
 
 def test_get_cart(mocker, user, app):
     mock_cache = mocker.patch("cachelib.RedisCache.get")
     mock_cache.return_value = ["1", "2", "3"]
 
-    assert get_cart(user_cart_key(user)) == ["1", "2", "3"]
+    assert get_cart(user) == ["1", "2", "3"]
